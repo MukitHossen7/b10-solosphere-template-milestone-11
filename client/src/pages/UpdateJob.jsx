aef2fae2a900d574f444ac70cb57d2586c1f0d95
MukitHossen7 <hossenmukit7@gmail.com>
2024-12-15 16:43:47 +0600

    update in data use params

--- client/src/pages/UpdateJob.jsx
+++ client/src/pages/UpdateJob.jsx
@@ -1,10 +1,15 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { format } from "date-fns";
+import { useContext, useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProvider";
+import toast from "react-hot-toast";
 
 const UpdateJob = () => {
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
   const [startDate, setStartDate] = useState(new Date());
   const [job, setJob] = useState({});
   const { id } = useParams();
@@ -18,7 +23,44 @@ const UpdateJob = () => {
     setJob(data);
     setStartDate(data?.deadline);
   };
-  console.log(job);
+  const handleUpdateJob = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const title = form.job_title.value;
+    const email = form.email.value;
+    const deadline = format(new Date(startDate), "P");
+    const category = form.category.value;
+    const min_price = parseInt(form.min_price.value);
+    const max_price = parseInt(form.max_price.value);
+    const description = form.description.value;
+    const updateData = {
+      title,
+      buyer: {
+        email: email,
+        name: user?.displayName,
+        photo: user?.photoURL,
+      },
+      deadline,
+      category,
+      min_price,
+      max_price,
+      description,
+      bid_count: job?.bid_count,
+    };
+    try {
+      const { data } = await axios.put(
+        `${import.meta.env.VITE_API_URL}/update-job/${id}`,
+        updateData
+      );
+      form.reset();
+      toast.success("Data update successfully");
+      navigate("/my-posted-jobs");
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+      toast.error("Failed to update data");
+    }
+  };
   return (
     <div className="flex justify-center items-center min-h-[calc(100vh-306px)] my-12">
       <section className=" p-2 md:p-6 mx-auto bg-white rounded-md shadow-md ">
@@ -26,7 +68,7 @@ const UpdateJob = () => {
           Update a Job
         </h2>
 
-        <form>
+        <form onSubmit={handleUpdateJob}>
           <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
             <div>
               <label className="text-gray-700 " htmlFor="job_title">
