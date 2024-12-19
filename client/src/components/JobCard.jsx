d2f89345f5ef73bf1b414d08c1ec577bf9254439
MukitHossen7 <hossenmukit7@gmail.com>
2024-12-19 10:02:57 +0600

    job details page design complete

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 
 const JobCard = ({ job }) => {
   const {
+    _id,
     title,
     deadline,
     category,
@@ -12,10 +13,9 @@ const JobCard = ({ job }) => {
     description,
     bid_count,
   } = job || {};
-
   return (
     <Link
-      to={`/job/1`}
+      to={`/job/${_id}`}
       className="w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md hover:scale-[1.05] transition-all"
     >
       <div className="flex items-center justify-between">
